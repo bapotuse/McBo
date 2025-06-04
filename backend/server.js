fbe83c6eeed2e05a50cbcad7a24661077d1663a9
bapotuse <77754709+bapotuse@users.noreply.github.com>
2025-06-04 22:10:48 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -46,10 +46,10 @@ function envoyerMail(to, subject, text) {
 
 
 const bdd = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'mcbo'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 });
 
 bdd.connect((err) => {
