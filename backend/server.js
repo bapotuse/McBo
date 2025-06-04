f0620689df943b4fe1f21aeef65e6a331dd76084
bapotuse <77754709+bapotuse@users.noreply.github.com>
2025-06-04 23:18:31 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const mysql = require('mysql2');
 const bcrypt = require('bcrypt');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.DB_PORT || 3000;
 const nodemailer = require('nodemailer');
 
 
